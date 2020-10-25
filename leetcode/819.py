afe17176f81a2bf001d8178e00de261764e565b1
ragdoll <museopkim0214@gmail.com>
2020-10-26 03:18:29 +0900

    Add LeetCode [#819] Solution

---
+++ leetcode/819.py
@@ -0,0 +1,14 @@
+import collections
+import re
+
+from typing import List
+
+
+# Solution
+class Solution:
+    def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+                 .lower().split() if word not in banned]
+
+        counts = collections.Counter(words)
+        return counts.most_common(1)[0][0]

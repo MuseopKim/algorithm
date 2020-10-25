179a9c9983b1ee0f796a4d6d7c754011ba3c06a1
ragdoll <museopkim0214@gmail.com>
2020-10-26 04:09:08 +0900

    Add LeetCode [#49] Solution

--- practice.py
+++ practice.py
@@ -1,7 +1,5 @@
-string = "A man, a plan, a canal: Panama"
-string_ignore_case = string.lower()
-string_seperated_by_blank = " ".join(string_ignore_case)
-print(string_seperated_by_blank)
-string_list_contain_blank = string_seperated_by_blank.split(" ")
-string_list_only_alphabet = []
-print(string_list_contain_blank)
+words = ["Word1", "Word2", "Word3"]
+
+for word in words:
+    print(sorted(word))
+    print(type(word))

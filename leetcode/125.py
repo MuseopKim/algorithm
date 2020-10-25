d158fe9fbe7cdf12d9c818e0d254586fff021cdd
ragdoll <museopkim0214@gmail.com>
2020-10-26 00:43:38 +0900

    Add Leetcode [#125] Solution_2

--- leetcode/125.py
+++ leetcode/125.py
@@ -1,3 +1,6 @@
+import collections
+from typing import Deque
+
 # Tried
 class Tried:
     def isPalindrome(self, s: str) -> bool:
@@ -31,5 +34,21 @@ class Solution1:
         while len(strs) > 1:
             if strs.pop(0) != strs.pop():
                 return False
+        return True
+
+
+# Solution 2
+class Solution2:
+    def isPalindrome(self, s: str) -> bool:
+        strs: Deque = collections.deque()
+
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
+
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+
         return True
 

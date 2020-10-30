2b9cf1c5936c83ded63d2892297feb4bf234b376
ragdoll <museopkim0214@gmail.com>
2020-10-31 02:45:44 +0900

    Add LeetCode [#5]

--- practice.py
+++ practice.py
@@ -1,5 +1,24 @@
-words = ["Word1", "Word2", "Word3"]
+from collections import deque
 
-for word in words:
-    print(sorted(word))
-    print(type(word))
+def bfs(graph, start, visited):
+    queue = deque([start])
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        print(v, end = ' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]

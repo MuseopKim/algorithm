f920feec56e00700e5ff3367883ef17ed06a3b5b
ragdoll <museopkim0214@gmail.com>
2021-10-13 23:49:14 +0900

    FindingMaze

---
+++ java/CommonTopics/FindingMaze.java
@@ -0,0 +1,45 @@
+package CommonTopics;
+
+import java.util.Scanner;
+
+public class FindingMaze {
+
+    static int[] dx = { -1, 0, 1, 0 };
+    static int[] dy = { 0, 1, 0, -1 };
+    static int[][] board;
+    static int count;
+
+    public static void DFS(int x, int y) {
+        if (x == 7 && y == 7) {
+            count += 1;
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && board[nx][ny] == 0) {
+                board[nx][ny] = 1;
+                DFS(nx, ny);
+                board[nx][ny] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        board = new int[8][8];
+
+        for (int i = 1; i < 8; i++) {
+            for (int j = 1; j < 8; j++) {
+                board[i][j] = stdIn.nextInt();
+            }
+        }
+
+        board[1][1] = 1;
+        DFS(1, 1);
+        System.out.println(count);
+    }
+}

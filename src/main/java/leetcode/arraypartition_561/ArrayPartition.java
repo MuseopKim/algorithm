997c79209dba1ef80310fb40085b53810f6cf874
Museop <museopkim0214@gmail.com>
2023-10-14 19:45:34 +0900

    add another solution

--- src/main/java/leetcode/arraypartition_561/ArrayPartition.java
+++ src/main/java/leetcode/arraypartition_561/ArrayPartition.java
@@ -1,6 +1,9 @@
 package leetcode.arraypartition_561;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * https://leetcode.com/problems/array-partition
@@ -13,6 +16,23 @@ public class ArrayPartition {
         int sum = 0;
         for (int i = 0; i <= nums.length - 2; i += 2) {
             sum += nums[i];
+        }
+
+        return sum;
+    }
+
+    public int solution1(int[] nums) {
+        int sum = 0;
+        List<Integer> pair = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int num : nums) {
+            pair.add(num);
+
+            if (pair.size() == 2) {
+                sum += Collections.min(pair);
+                pair.clear();
+            }
         }
 
         return sum;

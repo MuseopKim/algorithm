21d684a070487d533dcd93aab76cbfcab28dc6eb
Museop <museopkim0214@gmail.com>
2023-10-18 23:35:51 +0900

    [LeetCode 21] Merge Two Sorted Lists

---
+++ src/main/java/leetcode/mergetwosortedlists_21/MergeTwoSortedLists.java
@@ -0,0 +1,44 @@
+package leetcode.mergetwosortedlists_21;
+
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists
+ * https://leetcode.com/problems/merge-two-sorted-lists/solutions/2809815/java
+ */
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+
+        if (list2 == null) {
+            return list1;
+        }
+
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    private static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}

95a3794c479a12ded16cc22a47a09cbc0ceb26f6
Museop <museopkim0214@gmail.com>
2023-10-30 22:37:31 +0900

    [LeetCode 328] Odd Even Linked List

---
+++ src/main/java/leetcode/oddevenlinkedlist_328/OddEvenLinkedList.java
@@ -0,0 +1,43 @@
+package leetcode.oddevenlinkedlist_328;
+
+public class OddEvenLinkedList {
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode odd = head;
+        ListNode even = odd.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return head;
+    }
+
+    private static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
